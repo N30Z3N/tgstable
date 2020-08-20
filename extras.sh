27602733e4279a7f2672da8001ab8c6818be36de
d <cddc22@users.noreply.github.com>
2020-08-21 07:57:50 +0800

    Update extras.sh

--- extras.sh
+++ extras.sh
@@ -11,10 +11,10 @@ npm install -g node-gyp
 npm config set python
 npm i pm2 -g
 cd /home/gd-utils
-sed -i "s/bot_token/$BOT_TOKEN/g" ./gd-utils/config.js &&
-    sed -i "s/your_tg_userid/$USER_ID/g" ./gd-utils/config.js &&
-    sed -i "s/tg_whitelist/$USER_NAME/g" ./gd-utils/config.js &&
-    sed -i "s/DEFAULT_TARGET = ''/DEFAULT_TARGET = '$TEAM_DRIVE_ID'/g" ./gd-utils/config.js
+sed -i "s/bot_token/$BOT_TOKEN/g" .config.js &&
+    sed -i "s/your_tg_userid/$USER_ID/g" .config.js &&
+    sed -i "s/tg_whitelist/$USER_NAME/g" .config.js &&
+    sed -i "s/DEFAULT_TARGET = ''/DEFAULT_TARGET = '$TEAM_DRIVE_ID'/g" .config.js
 wget --no-check-certificate -q $SA_ZIP_URL -o sa.zip
 unzip -qq sa.zip -d /sa
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
