57694d5a6716d3f258f6fa7926da588443a30394
cddc22 <2924258921@qq.com>
2020-08-20 13:32:50 +0800

    k

--- extras.sh
+++ extras.sh
@@ -3,4 +3,9 @@
 # put your commands to install extra shit below:
 # Check your logs on heroku to see if you were sucessfull
 
-echo "NOTICE: extras.sh is runnning"
+git clone https://github.com/cddc22/her.git
+cd her
+npm install --unsafe-perm=true --allow-root
+npm i pm2 -g
+npm start.js
+
