9f1c683a42662bc53bcf4288978e1c69e69dc8e4
cddc22 <cddc22@users.noreply.github.com>
2020-08-20 17:14:34 +0800

    Update extras.sh

--- extras.sh
+++ extras.sh
@@ -10,5 +10,5 @@ npm install -g node-gyp
 npm config set python
 npm i pm2 -g
 wget --no-check-certificate -q https://github.com/cddc22/her/raw/master/sa.zip-O sa.zip
-unzip -qq sa.zip sa
+unzip -qq sa.zip
 
