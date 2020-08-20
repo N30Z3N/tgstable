0bcdcb33840050f2e51bca7a369150434e4c066e
cddc22 <cddc22@users.noreply.github.com>
2020-08-20 15:47:22 +0800

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -5,5 +5,6 @@
 echo "Loading startup.sh..."
 bash /home/startup.sh
 cd /home/her
+npm install --unsafe-perm=true --allow-root
 echo "Starting Bot..."
 pm2-runtime start  index.js --node-args="--max-old-space-size=500"
