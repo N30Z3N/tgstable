3513dcb261bd1c29e004cb8554857e4df26943fa
cddc22 <cddc22@users.noreply.github.com>
2020-08-20 17:21:35 +0800

    Update extras.sh

--- extras.sh
+++ extras.sh
@@ -9,6 +9,6 @@ nnpm config set unsafe-perm=true
 npm install -g node-gyp 
 npm config set python
 npm i pm2 -g
-wget --no-check-certificate -q https://github.com/cddc22/her/raw/master/sa.zip-O sa.zip
-unzip -qq sa.zip
+#wget --no-check-certificate -q https://github.com/cddc22/her/raw/master/sa.zip-O sa.zip
+unzip -qq sa.zip -d /sa
 
