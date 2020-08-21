f5a211e6aa12047b5827fde0d42ec9c73788e699
d <cddc22@users.noreply.github.com>
2020-08-21 08:12:26 +0800

    Update extras.sh

--- extras.sh
+++ extras.sh
@@ -2,19 +2,19 @@
 
 # put your commands to install extra shit below:
 # Check your logs on heroku to see if you were sucessfull
-git clone https://github.com/dissipator/gd-utils.git
-#git clone https://github.com/cddc22/her.git
-#cd her
-cd gd-utils
+#git clone https://github.com/dissipator/gd-utils.git
+git clone https://github.com/cddc22/her.git
+cd her
+#cd gd-utils
 nnpm config set unsafe-perm=true
 npm install -g node-gyp 
 npm config set python
 npm i pm2 -g
 cd /home/gd-utils
-sed -i "s/bot_token/$BOT_TOKEN/g" .config.js &&
+#sed -i "s/bot_token/$BOT_TOKEN/g" .config.js &&
     sed -i "s/your_tg_userid/$USER_ID/g" .config.js &&
     sed -i "s/tg_whitelist/$USER_NAME/g" .config.js &&
     sed -i "s/DEFAULT_TARGET = ''/DEFAULT_TARGET = '$TEAM_DRIVE_ID'/g" .config.js
-wget --no-check-certificate -q $SA_ZIP_URL -o sa.zip
+#wget --no-check-certificate -q $SA_ZIP_URL -o sa.zip
 unzip -qq sa.zip -d /sa
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
