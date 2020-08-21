f66ff5edc6132195cb3e5210187b801a6ab7acef
d <cddc22@users.noreply.github.com>
2020-08-21 08:36:49 +0800

    Update extras.sh

--- extras.sh
+++ extras.sh
@@ -18,5 +18,5 @@ sed -i "s/DEFAULT_TARGET = ''/DEFAULT_TARGET = '$TEAM_DRIVE_ID'/g" .config.js
 ls
 cat config.js
 #wget --no-check-certificate -q $SA_ZIP_URL -o sa.zip
-unzip -qq sa.zip -d /sa
+unzip -qq sa.zip -d /sa 
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
