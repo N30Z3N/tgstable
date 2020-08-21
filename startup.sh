8b22dd4ddb31836180e3ae13a00d58e7585658f5
d <cddc22@users.noreply.github.com>
2020-08-21 09:40:52 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,9 +4,9 @@
 # Check your logs on heroku to see if you were sucessfull
 
 echo "NOTICE: startup.sh is runnning"
-sed -i "s/bot_token/$BOT_TOKEN/g" .config.js &&
-sed -i "s/your_tg_userid/$USER_ID/g" .config.js 
-sed -i "s/tg_whitelist/$USER_NAME/g" .config.js
-sed -i "s/DEFAULT_TARGET = ''/DEFAULT_TARGET = '$TEAM_DRIVE_ID'/g" .config.js
+sed -i "s/bot_token/$BOT_TOKEN/g" config.js &&
+sed -i "s/your_tg_userid/$USER_ID/g" config.js 
+sed -i "s/tg_whitelist/$USER_NAME/g" config.js
+sed -i "s/DEFAULT_TARGET = ''/DEFAULT_TARGET = '$TEAM_DRIVE_ID'/g" config.js
 node clear-db.js
 cat config.js
