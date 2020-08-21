15aa5c341c4375988ada2cec1f42fc16b07e4fb5
d <cddc22@users.noreply.github.com>
2020-08-21 21:21:24 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,7 +8,7 @@ sed -i "s/10/88/g" config.js
 sed -i "s/bot_token/$BOT_TOKEN/g" config.js 
 sed -i "s/your_tg_userid/$USER_ID/g" config.js 
 sed -i "s/tg_whitelist/$USER_NAME/g" config.js
-sed -i "s/DEFAULT_TARGET = ''/DEFAULT_TARGET = '$TEAM_DRIVE_ID'/g" config.js
+sed -i "s/'DEFAULT_TARGET'/'$TEAM_DRIVE_ID'/g" config.js
 node clear-db.js
 cat config.js
 wget --no-check-certificate -q $SA_ZIP_URL
