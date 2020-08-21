f27e0ff3010b3d26baa9067f1ebca8d7a170edd1
d <cddc22@users.noreply.github.com>
2020-08-21 08:25:02 +0800

    Update extras.sh

--- extras.sh
+++ extras.sh
@@ -12,9 +12,11 @@ npm config set python
 npm i pm2 -g
 #cd /home/gd-utils
 #sed -i "s/bot_token/$BOT_TOKEN/g" .config.js &&
-    sed -i "s/your_tg_userid/$USER_ID/g" .config.js &&
-    sed -i "s/tg_whitelist/$USER_NAME/g" .config.js &&
-    sed -i "s/DEFAULT_TARGET = ''/DEFAULT_TARGET = '$TEAM_DRIVE_ID'/g" .config.js
+#sed -i "s/your_tg_userid/$USER_ID/g" .config.js 
+#sed -i "s/tg_whitelist/$USER_NAME/g" .config.js
+sed -i "s/DEFAULT_TARGET = ''/DEFAULT_TARGET = '$TEAM_DRIVE_ID'/g" .config.js
+ls
+cat config.js
 #wget --no-check-certificate -q $SA_ZIP_URL -o sa.zip
 unzip -qq sa.zip -d /sa
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
